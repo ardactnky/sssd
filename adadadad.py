0ab681f7eab6e30128e5a2110bd0810d61d4e2c3
vdi-terminal <vdi-terminal@EDU-WSB.local>
2026-03-30 12:48:05 +0200

    Initial commitd

---
+++ adadadad.py
@@ -0,0 +1,7 @@
+def greet(name):
+    """This function greets the person passed in as a parameter."""
+    message = "Hello, " + name + "!"
+    return message
+
+# Calling the function
+print(greet("Alice"))
---
+++ script.py
@@ -0,0 +1,5 @@
+def say_hi():
+    print("Hi there!")
+
+# To use it, just call its name:
+say_hi()
